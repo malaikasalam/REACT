8edef820958c18f76aedeba81ae3826d84553327
malaikasalam <malaikasalam61@gmail.com>
2025-09-03 02:22:22 +0500

    Delete codes directory

--- codes/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-// tailwind.config.js
-module.exports = {
-  darkMode: 'class', // enable class-based dark mode
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
